6cbe08706081aa75b949c7e245dce940d5a7e83b
Aman-Aggarwal <aman@intelligrape.com>
2010-09-11 12:13:29 +0530

    Added unit tests

--- test/unit/org/grails/plugins/dynamicConfig/ConfigPropertyTests.groovy
+++ test/unit/org/grails/plugins/dynamicConfig/ConfigPropertyTests.groovy
@@ -15,15 +15,39 @@ class ConfigPropertyTests extends GrailsUnitTestCase {
         super.tearDown()
     }
 
-    void testValid_STRING() {
+    void testCreate_Valid_STRING() {
         new ConfigProperty('x', 'y', DataType.STRING).save(failOnError:true);
         assertEquals (1, ConfigProperty.count())
         assertEquals ('y', CH.config.x)
     }
 
-    void testValid_INTEGER() {
+    void testCreate_Valid_INTEGER() {
         new ConfigProperty('x', '100', DataType.INTEGER).save(failOnError:true);
         assertEquals (1, ConfigProperty.count())
         assertEquals (100, CH.config.x)
+    }
+
+    void testCreate_Valid_LONG() {
+        new ConfigProperty('x', '100', DataType.LONG).save(failOnError:true);
+        assertEquals (1, ConfigProperty.count())
+        assertEquals (100, CH.config.x)
+    }
+
+    void testCreate_Valid_BOOLEAN() {
+        new ConfigProperty('x', 'false', DataType.BOOLEAN).save(failOnError:true);
+        assertEquals (1, ConfigProperty.count())
+        assertEquals (false, CH.config.x)
+    }
+
+    void testCreate_Valid_FLOAT() {
+        new ConfigProperty('x', '100.01', DataType.FLOAT).save(failOnError:true);
+        assertEquals (1, ConfigProperty.count())
+        assertEquals (100.01f, CH.config.x)
+    }
+
+    void testCreate_Valid_DOUBLE() {
+        new ConfigProperty('x', '100.01', DataType.DOUBLE).save(failOnError:true);
+        assertEquals (1, ConfigProperty.count())
+        assertEquals (100.01, CH.config.x)
     }
 }
