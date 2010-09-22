317293667618e7cabc6baec086214f14a0201a14
Aman-Aggarwal <aman@intelligrape.com>
2010-09-23 01:20:41 +0530

    Fixed all test-cases

--- grails-app/domain/org/grails/plugins/dynamicConfig/ConfigProperty.groovy
+++ grails-app/domain/org/grails/plugins/dynamicConfig/ConfigProperty.groovy
@@ -1,20 +1,18 @@
 package org.grails.plugins.dynamicConfig
 
 import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
+import org.codehaus.groovy.runtime.DefaultGroovyMethods
 
 class ConfigProperty {
 
-
     String name
     String value
-    DataType type
 
     ConfigProperty() {}
 
-    ConfigProperty(String name, String value, DataType type) {
+    ConfigProperty(String name, String value) {
         this.name = name
         this.value = value
-        this.type = type
     }
 
     static constraints = {
@@ -23,14 +21,7 @@ class ConfigProperty {
                 return 'configProperty.not.matches.message'
             }
         })
-        value(blank: false, validator: {String val, obj ->
-            switch (obj.type) {
-                case DataType.INTEGER: return (val.isInteger() ? true : 'configProperty.typeMismatch.java.lang.Integer'); break;
-                case DataType.LONG: return (val.isLong() ? true : 'configProperty.typeMismatch.java.lang.Long'); break;
-                case DataType.FLOAT: return (val.isFloat() ? true : 'configProperty.typeMismatch.java.lang.Float'); break;
-                case DataType.DOUBLE: return (val.isDouble() ? true : 'configProperty.typeMismatch.java.lang.Double'); break;
-            }
-        })
+        value(nullable: true)
     }
 
     String toString() {
@@ -46,26 +37,10 @@ class ConfigProperty {
     }
 
     void updateConfigMap() {
-        List propertyList = name.tokenize('.')
-        String lastProperty = propertyList.last()
-        propertyList.remove(lastProperty)
-        def currentMap = CH.config
-        propertyList?.each {
-            if (!currentMap[it]) { currentMap[it] = [:] }
-            currentMap = currentMap[it]
-        }
-        def configValue
-        if (value) {
-            switch (type) {
-                case DataType.BOOLEAN: configValue = value.toBoolean(); break;
-                case DataType.INTEGER: configValue = value.toInteger(); break;
-                case DataType.LONG: configValue = value.toLong(); break;
-                case DataType.FLOAT: configValue = value.toFloat(); break;
-                case DataType.DOUBLE: configValue = value.toDouble(); break;
-                default: configValue = value; break;
-            }
-            currentMap["${lastProperty}"] = configValue
-        }
+        Boolean useQuotes = !(((value ==~ /\[.*]/)) || DefaultGroovyMethods.isNumber(value) || DefaultGroovyMethods.isFloat(value) || value in ['true', 'false'])
+        String objectString = useQuotes ? "${name}='${value}'" : "${name}=${value}"
+        ConfigObject configObject = new ConfigSlurper().parse(objectString)
+        CH.setConfig(configObject)
     }
 
 }
--- test/unit/org/grails/plugins/dynamicConfig/ConfigPropertyTests.groovy
+++ test/unit/org/grails/plugins/dynamicConfig/ConfigPropertyTests.groovy
@@ -8,7 +8,6 @@ class ConfigPropertyTests extends GrailsUnitTestCase {
         super.setUp()
         mockDomain(ConfigProperty)
         mockConfig('')
-
     }
 
     protected void tearDown() {
@@ -16,175 +15,141 @@ class ConfigPropertyTests extends GrailsUnitTestCase {
     }
 
     void testCreate_Valid_STRING() {
-        new ConfigProperty('x', 'y', DataType.STRING).save();
+        new ConfigProperty('x', 'y').save();
         assertEquals(1, ConfigProperty.count())
         assertEquals('y', CH.config.x)
     }
 
+    void testCreate_Valid_LIST() {
+        new ConfigProperty('x', '[1,2,3]').save();
+        assertEquals(1, ConfigProperty.count())
+        assertEquals([1,2,3], CH.config.x)
+    }
+
+    void testCreate_Valid_MAP() {
+        new ConfigProperty('x', '[y : 1, z: "abc"]').save();
+        assertEquals(1, ConfigProperty.count())
+        assertEquals(1, CH.config.x.y)
+        assertEquals('abc', CH.config.x.z)
+    }
+
     void testCreate_Valid_INTEGER() {
-        new ConfigProperty('x', '100', DataType.INTEGER).save();
+        new ConfigProperty('x', '100').save();
         assertEquals(1, ConfigProperty.count())
         assertEquals(100, CH.config.x)
     }
 
-    void testCreate_Valid_LONG() {
-        new ConfigProperty('x', '100', DataType.LONG).save();
-        assertEquals(1, ConfigProperty.count())
-        assertEquals(100l, CH.config.x)
-    }
-
     void testCreate_Valid_BOOLEAN() {
-        new ConfigProperty('x', 'false', DataType.BOOLEAN).save();
+        new ConfigProperty('x', 'false').save();
         assertEquals(1, ConfigProperty.count())
         assertEquals(false, CH.config.x)
     }
 
-    void testCreate_Valid_FLOAT() {
-        new ConfigProperty('x', '100.01', DataType.FLOAT).save();
-        assertEquals(1, ConfigProperty.count())
-        assertEquals(100.01f, CH.config.x)
-    }
-
     void testCreate_Valid_DOUBLE() {
-        new ConfigProperty('x', '100.01', DataType.DOUBLE).save();
+        new ConfigProperty('x', '100.01').save();
         assertEquals(1, ConfigProperty.count())
         assertEquals(100.01, CH.config.x)
     }
 
     void testUpdate_Valid_STRING() {
-        ConfigProperty property = new ConfigProperty('x', 'y', DataType.STRING).save()
+        ConfigProperty property = new ConfigProperty('x', 'y').save()
         property.value = 'z'
         property.save()
         assertEquals('z', CH.config.x)
     }
 
     void testUpdate_Valid_INTEGER() {
-        ConfigProperty property = new ConfigProperty('x', '50', DataType.INTEGER).save();
+        ConfigProperty property = new ConfigProperty('x', '50').save();
         property.value = '100'
         property.save()
         assertEquals(100, CH.config.x)
     }
 
     void testUpdate_Valid_LONG() {
-        ConfigProperty property = new ConfigProperty('x', '50', DataType.LONG).save();
+        ConfigProperty property = new ConfigProperty('x', '50').save();
         property.value = '100'
         property.save()
-        assertEquals(100l, CH.config.x)
+        assertEquals(100, CH.config.x)
     }
 
     void testUpdate_Valid_BOOLEAN() {
-        ConfigProperty property = new ConfigProperty('x', 'false', DataType.BOOLEAN).save();
+        ConfigProperty property = new ConfigProperty('x', 'false').save();
         property.value = 'true'
         property.save()
         assertEquals(true, CH.config.x)
     }
 
-    void testUpdate_Valid_FLOAT() {
-        ConfigProperty property = new ConfigProperty('x', '50.01', DataType.FLOAT).save();
-        property.value = '100.01'
+    void testUpdate_Valid_DOUBLE() {
+        ConfigProperty property = new ConfigProperty('x', '50.01').save();
+        property.value = '100.01f'
         property.save()
         assertEquals(100.01f, CH.config.x)
     }
 
-    void testUpdate_Valid_DOUBLE() {
-        ConfigProperty property = new ConfigProperty('x', '50.01', DataType.DOUBLE).save();
-        property.value = '100.01'
-        property.save()
-        assertEquals(100.01, CH.config.x)
-    }
-
     void testCreate_Blank_Value() {
-        ConfigProperty property = new ConfigProperty('x', '', DataType.INTEGER)
+        ConfigProperty property = new ConfigProperty('x', '')
         property.save()
-        assertEquals(0, ConfigProperty.count())
-        assertEquals("blank", property.errors["value"])
-    }
-
-    void testCreate_Invalid_Integer() {
-        ConfigProperty property = new ConfigProperty('x', 'y', DataType.INTEGER)
-        property.save()
-        assertEquals(0, ConfigProperty.count())
-        assertEquals("configProperty.typeMismatch.java.lang.Integer", property.errors["value"])
-    }
-
-    void testCreate_Invalid_Float() {
-        ConfigProperty property = new ConfigProperty('x', 'y', DataType.FLOAT)
-        property.save()
-        assertEquals(0, ConfigProperty.count())
-        assertEquals("configProperty.typeMismatch.java.lang.Float", property.errors["value"])
-    }
-
-    void testCreate_Invalid_Long() {
-        ConfigProperty property = new ConfigProperty('x', 'y', DataType.LONG)
-        property.save()
-        assertEquals(0, ConfigProperty.count())
-        assertEquals("configProperty.typeMismatch.java.lang.Long", property.errors["value"])
-    }
-
-    void testCreate_Invalid_DOUBLE() {
-        ConfigProperty property = new ConfigProperty('x', 'y', DataType.DOUBLE)
-        property.save()
-        assertEquals(0, ConfigProperty.count())
-        assertEquals("configProperty.typeMismatch.java.lang.Double", property.errors["value"])
+        assertEquals(1, ConfigProperty.count())
+        assertEquals('', CH.config.x)
     }
 
     void testCreate_Two_Properties_Same_Name() {
-        ConfigProperty property1 = new ConfigProperty('x', 'y', DataType.STRING)
+        ConfigProperty property1 = new ConfigProperty('x', 'y')
         property1.save()
         assertEquals(1, ConfigProperty.count())
-        ConfigProperty property2 = new ConfigProperty('x', 'z', DataType.STRING)
+        ConfigProperty property2 = new ConfigProperty('x', 'z')
         property2.save()
         assertEquals(1, ConfigProperty.count())
         assertEquals("unique", property2.errors["name"])
     }
 
     void testCreate_Valid_Name2() {
-        ConfigProperty property = new ConfigProperty('firstName.lastName', 'y', DataType.STRING).save();
+        ConfigProperty property = new ConfigProperty('firstName.lastName', 'y').save();
         assertEquals(1, ConfigProperty.count())
     }
 
     void testCreate_Valid_Name3() {
-        new ConfigProperty('x_y', 'value1', DataType.STRING).save()
+        new ConfigProperty('x_y', 'value1').save()
         assertEquals(1, ConfigProperty.count())
         assertEquals('value1', CH.config.x_y)
     }
 
     void testCreate_Valid_Name4() {
-        new ConfigProperty('x.y', 'value1', DataType.STRING).save()
+        new ConfigProperty('x.y', 'value1').save()
         assertEquals(1, ConfigProperty.count())
         assertEquals('value1', CH.config.x.y)
     }
 
     void testCreate_Invalid_Name1() {
-        ConfigProperty property = new ConfigProperty('x y', 'value1', DataType.STRING)
+        ConfigProperty property = new ConfigProperty('x y', 'value1')
         property.save()
         assertEquals(0, ConfigProperty.count())
         assertEquals("configProperty.not.matches.message", property.errors["name"])
     }
 
     void testCreate_Invalid_Name2() {
-        ConfigProperty property = new ConfigProperty('x/y', 'value1', DataType.STRING)
+        ConfigProperty property = new ConfigProperty('x/y', 'value1')
         property.save()
         assertEquals(0, ConfigProperty.count())
         assertEquals("configProperty.not.matches.message", property.errors["name"])
     }
 
     void testCreate_Invalid_Name3() {
-        ConfigProperty property = new ConfigProperty('.x.y', 'value1', DataType.STRING)
+        ConfigProperty property = new ConfigProperty('.x.y', 'value1')
         property.save()
         assertEquals(0, ConfigProperty.count())
         assertEquals("configProperty.not.matches.message", property.errors["name"])
     }
 
     void testCreate_Invalid_Name4() {
-        ConfigProperty property = new ConfigProperty('x.y.', 'value1', DataType.STRING)
+        ConfigProperty property = new ConfigProperty('x.y.', 'value1')
         property.save()
         assertEquals(0, ConfigProperty.count())
         assertEquals("configProperty.not.matches.message", property.errors["name"])
     }
 
     void testCreate_Invalid_Name5() {
-        ConfigProperty property = new ConfigProperty('1x', 'value1', DataType.STRING)
+        ConfigProperty property = new ConfigProperty('1x', 'value1')
         property.save()
         assertEquals(0, ConfigProperty.count())
         assertEquals("configProperty.not.matches.message", property.errors["name"])
